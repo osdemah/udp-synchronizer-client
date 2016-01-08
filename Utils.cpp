aa4bbcee5ba81d7490ed10285d065ec3c68b0954
hamed1soleimani <hamed1soleimani@gmail.com>
2016-01-08 10:44:28 +0330

    add create dir and remove file/dir messages to client

--- Utils.cpp
+++ Utils.cpp
@@ -5,7 +5,7 @@
 #include "Utils.h"
 
 int Utils::print_usage(){
-    std::cerr << "Usage: client <host> <port> <message>" << std::endl;
+    std::cerr << "Usage: client <host> <port> <CREATE_FILE|CREATE_DIR|REMOVE_FILE|REMOVE_DIR> <file/dir>" << std::endl;
     return 1;
 }
 
--- main.cpp
+++ main.cpp
@@ -7,17 +7,26 @@ using boost::asio::ip::udp;
 
 int main(int argc, char *argv[]) {
     try {
-        if (argc != 4)
+        if (argc != 5)
         {
             return Utils::print_usage();
         }
 
         std::string port = Utils::get_parameter(argv[2]);
         std::string host = Utils::get_parameter(argv[1]);
-        std::string message = Utils::get_parameter(argv[3]);
+        std::string operation = Utils::get_parameter(argv[3]);
+        std::string filename = Utils::get_parameter(argv[4]);
 
-        if(port.empty() || host.empty() || message.empty())
+        if(port.empty() || host.empty() || operation.empty())
             return Utils::print_usage();
+
+        std::string message;
+        if(operation != "CREATE_FILE")
+            message = operation + ";;" + filename;
+        else {
+            std::cerr << "NOT SUPPORT YET!" << std::endl;
+            return Utils::print_usage();
+        }
 
         boost::asio::io_service io_service;
         UDPClient client{io_service, host, port};
