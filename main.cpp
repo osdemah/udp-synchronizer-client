62e4d74bcc62c92ce2bc390146fb448c6547aa76
hamed1soleimani <hamed1soleimani@gmail.com>
2016-01-13 23:25:10 +0330

    it isn't good idea to commit changes before testing but i can't test it on my machine!

--- main.cpp
+++ main.cpp
@@ -39,16 +39,18 @@ int main(int argc, char *argv[]) {
                     char * buffer = new char[512];
                     unsigned long pos = 0;
                     for(unsigned long i = 0; i < size / 512 + 1; i ++){
+                        std::string content;
                         if(i != size / 512) {
                             file.seekg(pos, std::ios::beg);
                             file.read(buffer, 512);
-                            std::string content{buffer};
+                            content = std::string{buffer};
                         }else{
                             int s = static_cast<int>(size - pos);
                             file.read(buffer, s);
-                            std::string content{buffer, s};
+                            content = std::string{buffer, s};
                         }
-                        std::string bare_message = filename + ";" + std::to_string(pos) + ";" + buffer;
+                        std::cout << content << std::endl;
+                        std::string bare_message = filename + ";" + std::to_string(pos) + ";" + content;
                         std::string hash_code = utils::hash_message(bare_message);
                         std::string message = hash_code + ";" + "CH;" + bare_message;
                         client.send_message(message);
