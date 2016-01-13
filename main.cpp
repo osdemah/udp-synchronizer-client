41702a887e73fcda4b91063477df2302114e1768
hamed1soleimani <hamed1soleimani@gmail.com>
2016-01-13 11:16:32 +0330

    add support of creation of a file

--- main.cpp
+++ main.cpp
@@ -7,8 +7,7 @@ using boost::asio::ip::udp;
 
 int main(int argc, char *argv[]) {
     try {
-        if (argc != 5)
-        {
+        if (argc != 5) {
             return utils::print_usage();
         }
 
@@ -17,18 +16,12 @@ int main(int argc, char *argv[]) {
         std::string operation = utils::get_parameter(argv[3]);
         std::string filename = utils::get_parameter(argv[4]);
 
-        if(port.empty() || host.empty() || operation.empty() || filename.empty() || !utils::check_operation(operation))
+        if (port.empty() || host.empty() || operation.empty() || filename.empty() || !utils::check_operation(operation))
             return utils::print_usage();
 
-        std::string message;
-        if(operation != "CREATE_FILE") {
-            message = operation + ";;" + filename;
-            std::string hash = utils::hash_message(message);
-            message = hash + ";;" + message;
-        }else {
-            std::cerr << "NOT SUPPORT YET!" << std::endl;
-            return utils::print_usage();
-        }
+        std::string message = operation + ";;" + filename;
+        std::string hash = utils::hash_message(message);
+        message = hash + ";;" + message;
 
         boost::asio::io_service io_service;
         UDPClient client{io_service, host, port};
